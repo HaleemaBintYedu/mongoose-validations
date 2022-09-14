cf1a7d6b1899adff1a8d1e311f4f21d14f73a5a2
Haleema Bint yedu <bintyeduhaleema@gmail.com>
2022-09-14 14:01:40 +0000

    Authentication

--- modules/users/auth.controller.js
+++ modules/users/auth.controller.js
@@ -1,4 +1,21 @@
 const User = require("./user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (user) => {
+  const token = jwt.sign(
+    { id: user._id, email: user.email },
+    "8d1c20af859dcebf401e383d03277bccc0d18466dbbea5216ab843ea4859c816",
+    {
+      expiresIn: "1h",
+    }
+  );
+
+  return {
+    token,
+    user,
+  };
+};
 
 exports.register = async (req, res) => {
   const { email, password } = req.body;
@@ -9,8 +26,45 @@ exports.register = async (req, res) => {
     return res.status(400).json({ error: "Email already in used" });
   }
 
-  //creating User
-  const user = await User.create({ ...req.body });
+  const hashedPassword = await bcrypt.hash(password, 12);
 
-  res.status(201).json({ user });
+  //creating User
+  const user = await User.create({ ...req.body, password: hashedPassword });
+
+  // generate token
+
+  const token = generateToken(user);
+
+  // const token = jwt.sign(
+  //   { id: user._id, email: user.email },
+  //   "8d1c20af859dcebf401e383d03277bccc0d18466dbbea5216ab843ea4859c816",
+  //   {
+  //     expiresIn: "1h",
+  //   }
+  // );
+
+  // const returnUser = {
+  //   token,
+  //   user,
+  // };
+
+  res.status(201).json({ token });
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  let user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ msg: "Invalid credentials" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ msg: "Invalid credentials" });
+  }
+
+  const token = generateToken(user);
+
+  res.status(200).json({ token });
 };
--- modules/users/auth.route.js
+++ modules/users/auth.route.js
@@ -1,8 +1,9 @@
 const router = require("express").Router;
-const { register } = require("./auth.controller");
+const { register, login } = require("./auth.controller");
 
 const authRouter = router();
 
+authRouter.post("/login", login);
 authRouter.post("/register", register);
 
 module.exports = { authRouter };
